ac5fd316d9e27d773e15a182ffda1cd372ebe29f
zAstrvl <xrean.aep@gmail.com>
2025-07-08 14:41:44 +0300

    Proje dosyası ekle.

---
+++ SchoolManagementSys/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolManagementSys.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
