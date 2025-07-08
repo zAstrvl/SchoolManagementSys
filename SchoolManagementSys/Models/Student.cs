3823737fad3eec32f5eeefb90f35c7779144ac00
zAstrvl <xrean.aep@gmail.com>
2025-07-08 15:19:02 +0300

    added student model and get controllers

---
+++ SchoolManagementSys/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolManagementSys.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public DateTime DateOfBirth { get; set; }
+    }
+}
