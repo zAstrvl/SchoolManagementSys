c875191db10d8719b2867b140ab82b547b7e5751
zAstrvl <xrean.aep@gmail.com>
2025-07-08 17:17:08 +0300

    added put and delete operations for student model

--- SchoolManagementSys/Controllers/StudentsController.cs
+++ SchoolManagementSys/Controllers/StudentsController.cs
@@ -45,10 +45,41 @@ namespace SchoolManagementSys.Controllers
         {
             var student = students.FirstOrDefault(s => s.Id == id);
             if(student == null)
-            {
                 return NotFound();
-            }
             return Ok(student);
+        }
+        [HttpPost]
+        public ActionResult<Student> AddStudent(Student newStudent)
+        {
+            if (newStudent == null || string.IsNullOrWhiteSpace(newStudent.Name) || string.IsNullOrWhiteSpace(newStudent.Surname))
+                return BadRequest("Invalid student data.");
+            newStudent.Id = students.Max(s => s.Id) + 1;
+            students.Add(newStudent);
+            return CreatedAtAction(nameof(GetStudentById), new { id = newStudent.Id }, newStudent);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, Student updatedStudent)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+            if (student == null)
+                return NotFound();
+
+            student.Id = updatedStudent.Id;
+            student.Name = updatedStudent.Name;
+            student.Surname = updatedStudent.Surname;
+            student.DateOfBirth = updatedStudent.DateOfBirth;
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+            if (student == null)
+                return NotFound();
+
+            students.Remove(student);
+            return NoContent();
         }
     }
 }
