f905685b5a8164eb930de7b26abf375e9772fc08
zAstrvl <xrean.aep@gmail.com>
2025-07-24 11:38:44 +0300

    removed unneccessary tables

--- SchoolManagementSys/Migrations/20250724083144_removed unneccessary tables.Designer.cs
+++ SchoolManagementSys/Migrations/20250724083144_removed unneccessary tables.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchoolManagementSys.Data;
 
@@ -10,9 +11,11 @@ using SchoolManagementSys.Data;
 namespace SchoolManagementSys.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20250724083144_removed unneccessary tables")]
+    partial class removedunneccessarytables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
